5fd19feb478472a40913410f69c34219a61586a7
Adelina-hub <adelina.macarascu@gmail.com>
2025-04-24 20:37:54 +0400

    sect finished

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,17 @@
-function Button() {
-  return <button></button>;
+import PropTypes from "prop-types";
+import styles from "./Button.module.css";
+
+Button.propTypes = {
+  children: PropTypes.node.isRequired,
+  onClick: PropTypes.func,
+  type: PropTypes.oneOf(["primary", "secondary"]),
+};
+function Button({ children, onClick, type }) {
+  return (
+    <button onClick={onClick} className={`${styles.btn} ${styles[type]}`}>
+      {children}
+    </button>
+  );
 }
 
 export default Button;
